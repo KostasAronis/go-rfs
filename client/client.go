179217b09688b7419d555f9dbccc1534738c9ca0
Kostas Aronis <aronis_k@yahoo.com>
2021-01-12 22:54:35 +0200

    placeholders for executables

---
+++ client/client.go
@@ -0,0 +1,8 @@
+/*
+https://www.cs.ubc.ca/~bestchai/teaching/cs416_2017w2/assign1/index.html
+https://www.cs.ubc.ca/~bestchai/teaching/cs416_2018w1/project1/index.html
+*/
+package main
+
+func main() {
+}
