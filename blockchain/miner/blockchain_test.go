d1d3d182e6488052d329fecf26735e05a691c976
Kostas Aronis <aronis_k@yahoo.com>
2021-01-20 02:45:18 +0200

    (almost) empty miner tests

---
+++ blockchain/miner/blockchain_test.go
@@ -0,0 +1,31 @@
+package miner_test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/KostasAronis/go-rfs/blockchain/miner"
+)
+
+func TestMiner(t *testing.T) {
+	minerConfig := miner.MinerConfig{
+		MinerID:             "1",
+		PeerMinersAddrs:     []string{"127.0.0.1:9002", "127.0.0.1:9003", "127.0.0.1:9004"},
+		IncomingClientsAddr: "127.0.0.1:8001",
+		IncomingMinersAddr:  "127.0.0.1:9001",
+		OutgoingMinersIP:    "127.0.0.1:10001",
+		CommonMinerConfig: miner.CommonMinerConfig{
+			GenOpBlockTimeout:      500,
+			MinedCoinsPerOpBlock:   3,
+			MinedCoinsPerNoOpBlock: 2,
+			NumCoinsPerFileCreate:  1,
+			PowPerOpBlock:          6,
+			PowPerNoOpBlock:        6,
+			ConfirmsPerFileCreate:  1,
+			ConfirmsPerFileAppend:  2,
+			GenesisBlockHash:       "",
+		},
+	}
+	miner := miner.New(&minerConfig)
+	log.Println(miner.Coins)
+}
