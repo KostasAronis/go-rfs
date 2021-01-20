6e8018d5a2e9063de1c385f85ce2f6fc93df5ff4
Kostas Aronis <aronis_k@yahoo.com>
2021-01-21 00:09:55 +0200

    small steps into implementing the actual blockchain

--- blockchain/blocktree.go
+++ blockchain/blocktree.go
@@ -1,5 +1,7 @@
 package blockchain
 
+import "errors"
+
 /*
 	blockchain
 	TODO: inser
@@ -11,8 +13,10 @@ type BlockTreeNode struct {
 }
 
 type BlockTree struct {
-	GenesisNode *BlockTreeNode
-	Nodes       []*BlockTreeNode
+	GenesisNode *Block
+	Blocks      []*Block
+	OpDiff      int
+	NoopDiff    int
 }
 
 func (b *BlockTree) GetLongestChain() {
@@ -22,6 +26,48 @@ func (b *BlockTree) GetLastBlock() *Block {
 	return nil
 }
 
-func (b *BlockTree) AppendBlock(block *Block) {
+func (b *BlockTree) AppendBlock(block *Block) error {
+	valid, err := b.ValidNode(block)
+	if err != nil {
+		return err
+	}
+	if !valid {
+		return errors.New("invalid block")
+	}
+	b.Blocks = append(b.Blocks, block)
+	return nil
+}
 
+func (b *BlockTree) getBlockByHash(hash string) *Block {
+	for _, block := range b.Blocks {
+		if block.GetComputedHash() == hash {
+			return block
+		}
+	}
+	return nil
+}
+
+func (b *BlockTree) ValidNode(block *Block) (bool, error) {
+	var diff int
+	if block.IsOp {
+		diff = b.OpDiff
+	} else {
+		diff = b.NoopDiff
+	}
+	isValid, err := block.ValidHash(diff)
+	if err != nil {
+		return false, err
+	}
+	if isValid {
+		if block.PrevHash != "" {
+			prevBlock := b.getBlockByHash(block.PrevHash)
+			if prevBlock.IsOp {
+				diff = b.OpDiff
+			} else {
+				diff = b.NoopDiff
+			}
+			return prevBlock.ValidHash(diff)
+		}
+	}
+	return false, nil
 }
