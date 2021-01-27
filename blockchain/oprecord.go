9dae5923023b06d30ab2eeb2e37e37c2cb721f5b
Kostas Aronis <aronis_k@yahoo.com>
2021-01-27 21:30:50 +0200

    records type changed

--- blockchain/oprecord.go
+++ blockchain/oprecord.go
@@ -1,9 +1,12 @@
 package blockchain
 
+import "github.com/KostasAronis/go-rfs/rfslib"
+
 //OpRecord describes an operation on the rfs stored on the blockchain
 type OpRecord struct {
+	UUID     string
 	OpType   OpType
 	MinerID  string
 	Filename string
-	Record   string
+	Record   *rfslib.Record
 }
