4f920882c0df8c11b1c88e7d869712b14a640d33
Kostas Aronis <aronis_k@yahoo.com>
2021-02-02 21:58:32 +0200

    timestamp on record

--- blockchain/oprecord.go
+++ blockchain/oprecord.go
@@ -1,12 +1,17 @@
 package blockchain
 
-import "github.com/KostasAronis/go-rfs/rfslib"
+import (
+	"time"
+
+	"github.com/KostasAronis/go-rfs/rfslib"
+)
 
 //OpRecord describes an operation on the rfs stored on the blockchain
 type OpRecord struct {
-	UUID     string
-	OpType   OpType
-	MinerID  string
-	Filename string
-	Record   *rfslib.Record
+	MinerID   string
+	Timestamp time.Time
+	UUID      string
+	OpType    OpType
+	Filename  string
+	Record    *rfslib.Record
 }
