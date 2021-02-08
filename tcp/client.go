247207f1e3888ce5e7abe7e49f18a9262704917b
Kostas Aronis <aronis_k@yahoo.com>
2021-02-08 02:11:56 +0200

    removed logs & fixed error handling on tcp client

--- tcp/client.go
+++ tcp/client.go
@@ -12,12 +12,14 @@ import (
 type Client struct {
 	ID          string
 	Address     string
-	Target      string
+	TargetAddr  string
+	TargetID    string
 	GovecLogger *govec.GoLog
 }
 
 //Send Sends tcp message to server
 func (c *Client) Send(msg *Msg) *Msg {
+	log.Printf("sending tcp to %s", c.TargetAddr)
 	if msg.ClientID == "" {
 		msg.ClientID = c.ID
 	}
@@ -28,15 +30,16 @@ func (c *Client) Send(msg *Msg) *Msg {
 		c.GovecLogger = govec.InitGoVector(c.ID, c.ID+"GoVector.log", goVecConfig)
 	}
 	vectorClockMessage := c.GovecLogger.PrepareSend("SendingMessage", msg, govec.GetDefaultLogOptions())
-	conn, err := net.Dial("tcp", c.Target)
+	conn, err := net.Dial("tcp", c.TargetAddr)
 	if err != nil {
 		log.Println(err)
+		return &Msg{
+			ClientID: c.ID,
+			MSGType:  Error,
+			Payload:  err.Error(),
+		}
 	}
-	// b, err := json.Marshal(msg)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	log.Printf("send: %+v\n", vectorClockMessage)
+	//log.Printf("send: %+v\n", vectorClockMessage)
 	n, err := conn.Write(vectorClockMessage)
 	if err != nil {
 		log.Println(err)
@@ -45,6 +48,6 @@ func (c *Client) Send(msg *Msg) *Msg {
 	n, err = conn.Read(resBytes)
 	res := Msg{}
 	json.Unmarshal(resBytes[0:n], &res)
-	log.Printf("recv: %+v\n", res)
+	//log.Printf("recv: %+v\n", res)
 	return &res
 }
