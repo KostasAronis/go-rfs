890e67d66aed4d8ed59e8172381084e006f016b4
Kostas Aronis <aronis_k@yahoo.com>
2021-02-15 00:30:37 +0200

    client not throwing on every error

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -35,7 +35,10 @@ func help() {
 // TODO: flesh out, currently just for testing
 func main() {
 	if len(os.Args) > 1 {
-		handleAction(os.Args...)
+		err := handleAction(os.Args...)
+		if err != nil {
+			panic(err)
+		}
 	} else {
 		reader := bufio.NewReader(os.Stdin)
 		for {
@@ -47,22 +50,37 @@ func main() {
 				return
 			}
 			arguments := strings.Split(text, " ")
-			handleAction(append([]string{""}, arguments...)...)
+			err := handleAction(append([]string{""}, arguments...)...)
+			if err != nil {
+				log.Println(err.Error())
+			}
 		}
 	}
 }
 
-func handleAction(args ...string) {
+func handleAction(args ...string) error {
 	action := args[1]
 	switch action {
 	case "save":
-		saveAndExit()
+		err := saveAndExit()
+		if err != nil {
+			return err
+		}
 	case "ls":
-		listFiles()
+		err := listFiles()
+		if err != nil {
+			return err
+		}
 	case "cat":
 		filename := args[2]
-		totalRecs := recCount(filename)
-		getRecords(filename, makeRange(0, totalRecs, totalRecs))
+		totalRecs, err := recCount(filename)
+		if err != nil {
+			return err
+		}
+		err = getRecords(filename, makeRange(0, totalRecs, totalRecs))
+		if err != nil {
+			return err
+		}
 	case "tail":
 		filename := args[2]
 		n := 5
@@ -70,12 +88,18 @@ func handleAction(args ...string) {
 			nArg, err := strconv.Atoi(args[3])
 			if err != nil {
 				help()
-				return
+				return nil
 			}
 			n = nArg
 		}
-		totalRecs := recCount(filename)
-		getRecords(filename, makeRange(totalRecs-n, totalRecs, totalRecs))
+		totalRecs, err := recCount(filename)
+		if err != nil {
+			return err
+		}
+		err = getRecords(filename, makeRange(totalRecs-n, totalRecs, totalRecs))
+		if err != nil {
+			return err
+		}
 	case "head":
 		filename := args[2]
 		n := 5
@@ -83,39 +107,50 @@ func handleAction(args ...string) {
 			nArg, err := strconv.Atoi(args[3])
 			if err != nil {
 				help()
-				return
+				return nil
 			}
 			n = nArg
 		}
-		totalRecs := recCount(filename)
+		totalRecs, err := recCount(filename)
+		if err != nil {
+			return err
+		}
 		getRecords(filename, makeRange(0, n, totalRecs))
 	case "append":
 		filename := args[2]
 		record := args[3]
-		appendRec(filename, record)
+		err := appendRec(filename, record)
+		if err != nil {
+			return err
+		}
 	case "touch":
 		filename := args[2]
-		touch(filename)
+		err := touch(filename)
+		if err != nil {
+			return err
+		}
 	default:
 		help()
-		return
+		return nil
 	}
+	return nil
 }
 
-func saveAndExit() {
+func saveAndExit() error {
 	msg := tcp.Msg{
 		MSGType: tcp.StoreAndStop,
 		Payload: map[string]interface{}{},
 	}
 	res, err := send(&msg)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	resMap := res.(map[string]interface{})
 	filename := resMap["Filename"]
 	log.Printf("Stored in file: %s", filename)
+	return nil
 }
-func getRecords(filename string, indexes []int) {
+func getRecords(filename string, indexes []int) error {
 	msg := tcp.Msg{
 		MSGType: tcp.ReadRec,
 		Payload: map[string]interface{}{
@@ -125,7 +160,7 @@ func getRecords(filename string, indexes []int) {
 	}
 	res, err := send(&msg)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	recArr := res.([]interface{})
 	for _, rec := range recArr {
@@ -133,6 +168,85 @@ func getRecords(filename string, indexes []int) {
 		r.FromFloatArrayInterface(rec)
 		log.Println(r.ToString())
 	}
+	return nil
+}
+
+func recCount(filename string) (int, error) {
+	msg := tcp.Msg{
+		MSGType: tcp.TotalRecs,
+		Payload: map[string]interface{}{
+			"Filename": filename,
+		},
+	}
+	res, err := send(&msg)
+	if err != nil {
+		return 0, err
+	}
+	return int(res.(float64)), nil
+}
+
+func listFiles() error {
+	msg := tcp.Msg{
+		MSGType: tcp.ListFiles,
+	}
+	res, err := send(&msg)
+	if err != nil {
+		return err
+	}
+	log.Println(res)
+	return nil
+}
+
+func appendRec(filename string, record string) error {
+	msg := tcp.Msg{
+		MSGType: tcp.AppendRec,
+		Payload: map[string]interface{}{
+			"Filename": filename,
+			"Record":   strToRec(record),
+		},
+	}
+	res, err := send(&msg)
+	if err != nil {
+		return err
+	}
+	log.Println(res)
+	return nil
+}
+func touch(filename string) error {
+	msg := tcp.Msg{
+		MSGType: tcp.CreateFile,
+		Payload: map[string]interface{}{
+			"Filename": filename,
+			"Record":   strToRec(""),
+		},
+	}
+	res, err := send(&msg)
+	if err != nil {
+		return err
+	}
+	log.Println(res)
+	return nil
+}
+
+func send(msg *tcp.Msg) (interface{}, error) {
+	c := tcp.Client{
+		ID:         "c_1",
+		Address:    "who cares",
+		TargetAddr: ":8001",
+		TargetID:   "1",
+	}
+	res := c.Send(msg, "")
+	if res.MSGType == tcp.Error {
+		return nil, fmt.Errorf(res.Payload.(string))
+	}
+	return res.Payload, nil
+}
+
+func strToRec(str string) *rfslib.Record {
+	r := rfslib.Record{}
+	r.FromString(str)
+	//copy(r[:], str[:])
+	return &r
 }
 
 func makeRange(min, max, total int) []int {
@@ -147,79 +261,4 @@ func makeRange(min, max, total int) []int {
 		a[i] = min + i
 	}
 	return a
-}
-
-func recCount(filename string) int {
-	msg := tcp.Msg{
-		MSGType: tcp.TotalRecs,
-		Payload: map[string]interface{}{
-			"Filename": filename,
-		},
-	}
-	res, err := send(&msg)
-	if err != nil {
-		panic(err)
-	}
-	return int(res.(float64))
-}
-
-func listFiles() {
-	msg := tcp.Msg{
-		MSGType: tcp.ListFiles,
-	}
-	res, err := send(&msg)
-	if err != nil {
-		panic(err)
-	}
-	log.Println(res)
-}
-
-func appendRec(filename string, record string) {
-	msg := tcp.Msg{
-		MSGType: tcp.AppendRec,
-		Payload: map[string]interface{}{
-			"Filename": filename,
-			"Record":   strToRec(record),
-		},
-	}
-	res, err := send(&msg)
-	if err != nil {
-		panic(err)
-	}
-	log.Println(res)
-}
-func touch(filename string) {
-	msg := tcp.Msg{
-		MSGType: tcp.CreateFile,
-		Payload: map[string]interface{}{
-			"Filename": filename,
-			"Record":   strToRec(""),
-		},
-	}
-	res, err := send(&msg)
-	if err != nil {
-		panic(err)
-	}
-	log.Println(res)
-}
-
-func send(msg *tcp.Msg) (interface{}, error) {
-	c := tcp.Client{
-		ID:         "c_1",
-		Address:    "who cares",
-		TargetAddr: ":8001",
-		TargetID:   "1",
-	}
-	res := c.Send(msg)
-	if res.MSGType == tcp.Error {
-		return nil, fmt.Errorf(res.Payload.(string))
-	}
-	return res.Payload, nil
-}
-
-func strToRec(str string) *rfslib.Record {
-	r := rfslib.Record{}
-	r.FromString(str)
-	//copy(r[:], str[:])
-	return &r
 }
