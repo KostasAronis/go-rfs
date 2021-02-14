656497ccf73e478527e3944eea85332b74755f3a
Kostas Aronis <aronis_k@yahoo.com>
2021-02-15 00:28:16 +0200

    string method for optype enum

--- blockchain/optype.go
+++ blockchain/optype.go
@@ -11,3 +11,14 @@ const (
 	//AppendRec operation on the blockchain
 	AppendRec OpType = OpType(tcp.AppendRec)
 )
+
+func (t OpType) String() string {
+	switch t {
+	case CreateFile:
+		return "CreateFile"
+	case AppendRec:
+		return "AppendRec"
+	default:
+		return "UnknownMsg"
+	}
+}
